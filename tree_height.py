3a5ef154e28f0d95b0234de82ce8793a2fd9c510
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-03-05 13:53:13 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -9,12 +9,20 @@ def compute_height(n, parents):
     heights = [0] * n
     max_height = 0
     
+    def compute_height_from_parent(parent, heights):
+        if heights[parent] > 0:
+            return heights[parent]
+        else:
+            height = compute_height_from_parent(parents[parent], heights) + 1
+            height[parent] = height
+            return height
+    
     for i in range(n):
         parent = parents[i]
         if parent == -1:
             height = 1
         else:
-            height = height[parent] + 1
+            height = compute_height_from_parent(parent, heights) + 1
         heights[i] = height
         max_height = max(max_height, height)
     return max_height
