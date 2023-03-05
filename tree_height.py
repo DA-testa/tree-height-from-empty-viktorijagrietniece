2d1b10ad01565dc269406c3779d9fd05904e1547
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-03-05 14:14:25 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -19,30 +19,21 @@ def compute_height(n, parents):
     for i in range(n):
         height(i)
     return int(max(parent))
-    
-    # Write this function
-    # max_height = 0
-    # Your code here
-    # return max_height
-
 
 def main():
-    filename = input()
-    if filename and "a" not in filename:
-        try:
-            with open('inputs/' + filename, 'r') as f:
-                n = int(f.readline())
-                parents = list(map(int, f.readline().split()))
-        except:
+    letter = input()
+    if letter == "F":
+       filename = input()
+       if filename and "a" not in filename:
+           with open('inputs/' + filename, 'r') as f:
+               n = int(f.readline())
+               parents = list(map(int, f.readline().split()))
+        else:
             return
-    else:
-        try:
-            n = int(input())
-            s = input().strip()
-            parents = [int(x) for x in s.split()]
-        except ValueError:
-            return
-        
+    elif letter == "I":
+        n = int(input())
+        parents = list(map(int, input().split()))
+    else:  
     print(compute_height(n, parents))
 
 sys.setrecursionlimit(10**7)  # max depth of recursion
