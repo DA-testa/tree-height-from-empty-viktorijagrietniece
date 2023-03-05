52fc120e9ebb4eb6495bddf39cef5d354935b8d6
viktorijagrietniece <123946342+viktorijagrietniece@users.noreply.github.com>
2023-03-05 15:47:59 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,51 +6,37 @@ import numpy
 
 
 def compute_height(num_nodes, parents):
-    parent = [-1] * num_nodes
-    root = -1
-    
-    for i in range(num_nodes):
-        if parents[i] == -1:
-            root = i
-            break
-        else:
-            parent[i] = parents[i]
+    parent = numpy.zeros(num_nodes)
     
     def height(i):
-        if parent[i] != -1:
+        if parent[i] != -0:
             return parent[i]
-        if i == root:
+        if parents[i] == -1:
             parent[i] = 1
         else:
-            parent[i] = height(parent[i])+1
+            parent[i] = height(parents[i])+1
         return parent[i]
         
     for i in range(num_nodes):
         height(i)
-    return max(parent)
+    return int(max(parent))
 
 def main():
-    n = 0
-    parents = []
-    try:        
-        letter = input()
-        if letter == "F":
-            filename = input()
-            try:
-                with open('inputs/' + filename, 'r') as f:
-                    n = int(f.readline())
-                    parents = list(map(int, f.readline().split()))
-            except FileNotFoundError:
-                return
+    letter = input()
+    if letter == "F":
+        filename = input()
+        if "a" not in filename:
+            with open('inputs/' + filename, 'r') as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+        else:
+            return
         elif letter == "I":
             n = int(input())
             parents = list(map(int, input().split()))
         else:  
-            n = int(input())
-        parents = list(map(int, input().split()))
-        print(compute_height(n, parents))
-    except EOFError:
-        pass
+            print(compute_height(n, parents))
+   
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
